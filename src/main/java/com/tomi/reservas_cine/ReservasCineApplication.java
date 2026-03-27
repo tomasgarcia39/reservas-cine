b126c5c70e5bc3abc249293c6a752e240765d43d
Tomas Garcia <tomas.garcia.col17@gmail.com>
2026-03-27 18:14:59 -0300

    Initial project setup with Spring Boot, JPA, PostgreSQL and Validation dependencies

---
+++ src/main/java/com/tomi/reservas_cine/ReservasCineApplication.java
@@ -0,0 +1,13 @@
+package com.tomi.reservas_cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasCineApplication.class, args);
+	}
+
+}
